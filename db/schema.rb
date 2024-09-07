20892b31a569d57b4facca1c7770f106480ad01a
Nagai_sho <as.uland.dr@gmail.com>
2024-09-07 15:13:44 +0900

    rails db:migrate の実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2024_09_07_060717) do
+  create_table "orders", charset: "utf8mb3", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
