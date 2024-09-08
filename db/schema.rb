dc2856e002e67ff5877c57bac2c930ab6edfceb2
Nagai_sho <as.uland.dr@gmail.com>
2024-09-09 05:45:07 +0900

    テンプレートフォルダ取り込み

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_09_07_060717) do
+ActiveRecord::Schema[7.0].define(version: 2024_09_08_203209) do
+  create_table "items", charset: "utf8mb3", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "orders", charset: "utf8mb3", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
