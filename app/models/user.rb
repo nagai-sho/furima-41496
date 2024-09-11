15acd718af7d96b08532c82fb9532ca025093730
Nagai_sho <as.uland.dr@gmail.com>
2024-09-11 17:52:45 +0900

    validatesの実装、テストコードの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,30 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :nickname
+    validates :last_name, format: {
+      with: /\A[ぁ-んァ-ヶ一-龥々一]+\z/
+      # message: 'は漢字、ひらがな、カタカナのみで入力してください'
+    }
+    validates :last_name_kana, format: {
+      with: /\A[ァ-ヶ一]+\z/
+      # message: 'はカタカナのみで入力してください'
+    }
+    validates :first_name, format: {
+      with: /\A[ぁ-んァ-ヶ一-龥々一]+\z/
+      # message: 'は漢字、ひらがな、カタカナのみで入力してください'
+    }
+    validates :first_name_kana, format: {
+      with: /\A[ァ-ヶ一]+\z/
+      # message: 'はカタカナのみで入力してください'
+    }
+    validates :birthday
+  end
+
+  validates :password, format: {
+    with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,}\z/i
+    # message: 'は英数字両方必須、６文字以上で入力してください'
+  }
 end
---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname              { Faker::Name.initials(number: 2) }
+    email                 { Faker::Internet.email }
+    password              { Faker::Internet.password(min_length: 6) }
+    password_confirmation { password }
+    last_name             { 'やマ田' }
+    last_name_kana        { 'ヤマダ' }
+    first_name            { '太ろウ' }
+    first_name_kana       { 'タロウ' }
+    birthday              { Faker::Date.between(from: '1930-01-01', to: '2017-12-31') }
+  end
+end
