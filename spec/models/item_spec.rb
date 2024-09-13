46fc5e24fdffa975b705251e1c303c2c6e86425d
Nagai_sho <as.uland.dr@gmail.com>
2024-09-13 16:12:05 +0900

    単体テストコードの記述

---
+++ spec/models/item_spec.rb
@@ -0,0 +1,67 @@
+require 'rails_helper'
+
+RSpec.describe Item, type: :model do
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe 'item投稿機能の実装' do
+    context 'itemの新規投稿ができる' do
+      it '全ての項目が入力されていれば投稿ができる' do
+        expect(@item).to be_valid
+      end
+    end
+    context 'itemの新規投稿ができない' do
+      it '画像が無ければ投稿ができない' do
+        @item.image.purge
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+      it '商品名が無ければ投稿ができない' do
+        @item.product = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Product can't be blank")
+      end
+      it '商品の説明が無ければ投稿ができない' do
+        @item.explain = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Explain can't be blank")
+      end
+      it '商品の状態が選択されていなければ投稿ができない' do
+        @item.condition_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Condition can't be blank")
+      end
+      it '配送料が選択されていなければ投稿ができない' do
+        @item.shipping_fee_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping fee can't be blank")
+      end
+      it '都道府県が選択されていなければ投稿ができない' do
+        @item.prefecture_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture can't be blank")
+      end
+      it '配送までの日数が選択されていなければ投稿ができない' do
+        @item.required_number_of_day_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Required number of day can't be blank")
+      end
+      it '価格の入力が無ければ投稿ができない' do
+        @item.price = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+      it '価格が入力可能範囲外だと投稿ができない' do
+        @item.price = 100
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price must be greater than or equal to 300')
+      end
+      it '価格が全角だと投稿ができない' do
+        @item.price = '１００'
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price is not a number')
+      end
+    end
+  end
+end
