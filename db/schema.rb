bc13ee849b65337b12b9ea09e045b3f50abe410e
Nagai_sho <as.uland.dr@gmail.com>
2024-09-16 06:50:05 +0900

    購入内容の表示

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_09_12_010108) do
+ActiveRecord::Schema[7.0].define(version: 2024_09_15_165717) do
   create_table "active_storage_attachments", charset: "utf8mb3", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -67,6 +67,10 @@ ActiveRecord::Schema[7.0].define(version: 2024_09_12_010108) do
   create_table "orders", charset: "utf8mb3", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.bigint "item_id", null: false
+    t.index ["item_id"], name: "index_orders_on_item_id"
+    t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
   create_table "prefectures", charset: "utf8mb3", force: :cascade do |t|
@@ -105,4 +109,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_09_12_010108) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "items", "users"
+  add_foreign_key "orders", "items"
+  add_foreign_key "orders", "users"
 end
