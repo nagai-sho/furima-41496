c8b1b544afef85a41c75df609fd8251927af4aab
Nagai_sho <as.uland.dr@gmail.com>
2024-09-16 08:33:49 +0900

    購入ページへの遷移のvalidation

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,9 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
   def index
     @item = Item.find(params[:item_id])
+    return if current_user == @item.user_id
+
+    redirect_to root_path
   end
 end
