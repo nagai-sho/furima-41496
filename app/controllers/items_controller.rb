118b3b1e412bc2961080376e1de009fbd12c752d
Nagai_sho <as.uland.dr@gmail.com>
2024-09-15 16:45:19 +0900

    商品削除機能のCB修正（１）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   def index
     @items = Item.order(created_at: :desc)
@@ -41,11 +41,10 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    item = Item.find(params[:id])
-    if item.destroy
+    if @item.destroy
       redirect_to root_path
     else
-      render :edit, status: :unprocessable_entit
+      render :show, status: :unprocessable_entit
     end
   end
 
