5efc595d8c4eb97a429aa5e47a9d1cd3d1e4471d
Nagai_sho <as.uland.dr@gmail.com>
2024-09-16 11:21:03 +0900

    ddressesテーブルの追加

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,8 +2,24 @@ class OrdersController < ApplicationController
   before_action :authenticate_user!
   def index
     @item = Item.find(params[:item_id])
-    return if current_user == @item.user_id
+    if current_user == @item.user_id
+      redirect_to root_path
+    else
+    end
+    @order = Order.new
+  end
 
-    redirect_to root_path
+  def create
+    @item = Item.find(params[:item_id])
+    @order = Order.create(order_params)
+  end
+
+  def private
+    params.require(:order).permit(
+      :number
+    ).merge(
+      user_id: current_user.id,
+      item_id: @item_id
+    )
   end
 end
