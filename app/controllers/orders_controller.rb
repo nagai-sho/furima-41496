25f9ff645ce2004799248c7c53d88f13942a7d32
Nagai_sho <as.uland.dr@gmail.com>
2024-09-17 07:25:53 +0900

    商品購入機能の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,10 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
   def index
+    gon.public_key = ENV['PAYJP_PUBLIC_KEY']
     @order_address = OrderAddress.new
     @item = Item.find(params[:item_id])
-    if current_user == @item.user_id
+    if current_user.id == @item.user_id
       redirect_to root_path
     else
     end
@@ -11,29 +12,42 @@ class OrdersController < ApplicationController
   end
 
   def create
+    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
+      pay_item(@order_address.token)
       @order_address.save
       redirect_to root_path
     else
-      render :index, status: :enprocessable_entity
+      gon.public_key = ENV['PAYJP_PUBLIC_KEY']
+      render :index, status: :unprocessable_entity
     end
-    # @order = Order.create(order_params)
-    # Address.create(address_params)
   end
 
   private
 
+  def pay_item(token)
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    Payjp::Charge.create(
+      amount: @item.price, # 商品の値段
+      card: order_params[:token], # 　カードトークン
+      currency: 'jpy' # 通貨の種類
+    )
+  end
+
   def order_params
-    params.require(:order_adress)
+    params.require(:order_address)
           .permit(
-            :price,
             :post_code,
             :prefecture_id,
             :municipality,
             :house_number,
             :building,
             :phone_number
-          ).merge(user_id: current_user.id)
+          ).merge(
+            user_id: current_user.id,
+            item_id: params[:item_id],
+            token: params[:token]
+          )
   end
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,21 +1,23 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :price,
-                :post_code,
+  attr_accessor :post_code,
                 :prefecture_id,
                 :municipality,
                 :house_number,
                 :building,
                 :phone_number,
                 :user_id,
-                :order_id
+                :item_id,
+                :token
+
+  # :price
 
   with_options presence: true do
-    validates :price, numericality: {
-      greater_than_or_equal_to: 300,
-      less_than_or_equal_to: 9_999_999,
-      only_integer: true
-    }
+    # validates :price, numericality: {
+    #   greater_than_or_equal_to: 300,
+    #   less_than_or_equal_to: 9_999_999,
+    #   only_integer: true
+    # }
     validates :post_code, format: {
       with: /\A[0-9]{3}-[0-9]{4}\z/
     }
@@ -23,13 +25,19 @@ class OrderAddress
       other_than: 1, message: "can't be blank"
     }
     validates :user_id
+    validates :item_id
     validates :municipality
     validates :house_number
     validates :phone_number
+    validates :token
   end
 
   def save
-    order = Order.create(price:, user_id:)
+    order = Order.create(
+      # price:,
+      user_id:,
+      item_id:
+    )
     Address.create(
       post_code:,
       prefecture_id:,
